7d1695da905971502e2b8f783e7df4c918367e0d
Chris Anthes <chris@hapnyn.com>
2018-07-24 11:49:51 -0700

    Add statuses to tools

---
+++ tools/statuses.js
@@ -0,0 +1,10 @@
+export const statuses = [
+  'ED',   // Editors' Draft (not a W3C Technical Report)
+  'FPWD', // First Public Working Draft
+  'WD',   // Working Draft
+  'LC',   // Last Call Working Draft
+  'CR',   // Candidate Recommendation
+  'PR',   // Proposed Recommendation
+  'REC',  // Recommendation
+  'NOTE', // Working Group Note
+]
